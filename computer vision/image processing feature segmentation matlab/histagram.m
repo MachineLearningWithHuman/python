8a02566680d0fb8cd75cfef59b63e9ddd06f991c
satyajit maitra <50560424+MachineLearningWithHuman@users.noreply.github.com>
2020-04-02 15:09:35 +0530

    update

--- computer vision/image processing feature segmentation matlab/histagram.m
+++
@@ -1,5 +0,0 @@
-img = imread('cameraman.tif');
-%evaluate histogram of the image and store it in the variable - histogram
-histogram=imhist(img);
-
-plot(histogram)
--- computer vision/image processing feature segmentation matlab/image_negetive.m
+++
@@ -1,8 +0,0 @@
-img = imread('pout.tif');
-%Modify the code below. Compute Image Negative
-img_neg = imcomplement(img)
-
-figure
-imshow(img)
-figure
-imshow(img_neg)
